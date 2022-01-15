69dd5e4001d80f9eefd3233d8541e5edd33d8a89
Kazuhiro Mimaki <godblessu0405@gmail.com>
2022-01-15 16:07:04 +0900

    fix: c0487c2661810cc3b88bb5f10fbef12ea03ea442

--- 01-15.container-with-most-water.py
+++ 01-15.container-with-most-water.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxArea(self, height_list: List[int]) -> int:
         maximum = 0
-        for i in range(len(height_list)):
-            target_range = len(height_list) - (i+1)
-            for j in range(i+1, target_range+i+1):
-                width = j - i
-                # iが始点, jが終点
-                if height_list[i] < height_list[j]:
-                    height = height_list[i]
-                elif height_list[i] >= height_list[j]:
-                    height = height_list[j]
-                area = height * width
-                if area > maximum:
-                    maximum = area
+        left, right = 0, len(height_list) - 1
+        while left < right:
+            height = min(height_list[left], height_list[right])
+            width = right - left
+            area = height * width
+            if maximum < area:
+                maximum = area
+            if height_list[left] < height_list[right]:
+                left += 1
+            elif height_list[left] >= height_list[right]:
+                right -= 1
         return maximum
