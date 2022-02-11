6ca3b344ce881252b56f5886db196ad106c7ad8d
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-02-11 10:16:24 +0900

    02-11 Climbing Stairs

---
+++ 02-11.climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2: 
+            return n
+        
+        a, b, c = 0, 1, 2
+        
+        while (n > 2):
+            a, b = b, c
+            c = a + b
+            n = n - 1
+        
+        return c
