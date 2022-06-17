5059b9e195d3d3360d9b5580c62a976a8df16363
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-06-18 08:20:33 +0900

    06-18 Swap Nodes in Pairs

---
+++ 06-18.swap-nodes-in-pairs.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(val=0, next=head)
+        curr = dummy
+        while curr.next and curr.next.next:
+            first = curr.next
+            second = curr.next.next
+            curr.next = second
+            first.next = second.next
+            second.next = first
+            curr = curr.next.next
+        return dummy.next
